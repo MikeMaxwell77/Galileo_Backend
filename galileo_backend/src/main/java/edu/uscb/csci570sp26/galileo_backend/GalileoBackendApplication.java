f7a84418987947ed7077bf244f23d4c0cde96cc7
dscheer <dscheer@BEUSCB4388.ds.sc.edu>
2026-03-27 11:51:29 -0400

    initial commit

---
+++ galileo_backend/src/main/java/edu/uscb/csci570sp26/galileo_backend/GalileoBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.uscb.csci570sp26.galileo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalileoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalileoBackendApplication.class, args);
+	}
+
+}
---
+++ galileo_backend/src/test/java/edu/uscb/csci570sp26/galileo_backend/GalileoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uscb.csci570sp26.galileo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalileoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
