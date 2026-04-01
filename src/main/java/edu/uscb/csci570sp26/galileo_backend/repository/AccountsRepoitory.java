b0a63408b1d527a70f3bf412f640b99b9bfaa684
mm401@email.uscb.edu <118575021+MikeMaxwell77@users.noreply.github.com>
2026-04-01 10:36:55 -0400

    Created the tables in pgadmin4

--- src/main/java/edu/uscb/csci570sp26/galileo_backend/repository/AccountsRepoitory.java
+++
@@ -1,7 +0,0 @@
-package edu.uscb.csci570sp26.galileo_backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public class AccountsRepoitory extends JpaRepository<Accounts, Long>{
-
-}
