4859d1f10ab453b076a820759e4f81aa84052b90
mm401@email.uscb.edu <118575021+MikeMaxwell77@users.noreply.github.com>
2026-04-02 14:54:46 -0400

    Added Account Controller for new Accounts and registered an accout through postman

---
+++ src/main/java/edu/uscb/csci570sp26/galileo_backend/controller/AccountController.java
@@ -0,0 +1,61 @@
+package edu.uscb.csci570sp26.galileo_backend.controller;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort; // needed for sorting list of users by ID
+import org.springframework.web.bind.annotation.*;
+
+//import edu.uscb.csci570sp26.galileo_backend.exception.UserNotFoundException;
+import edu.uscb.csci570sp26.galileo_backend.model.Accounts;
+import edu.uscb.csci570sp26.galileo_backend.repository.AccountsRepository;
+
+
+@RestController
+public class AccountController {
+
+	
+	@Autowired
+	private AccountsRepository accountsRepository;
+
+	
+	@PostMapping("/account")
+	Accounts newAccount(@RequestBody Accounts newAccount) {
+		return accountsRepository.save(newAccount);
+	}
+
+	@GetMapping("/accounts")
+	List<Accounts> getAllAccounts() {
+		return accountsRepository.findAll(Sort.by(Sort.Direction.ASC, "id"));// sort users by ID in ascending order
+	}
+
+	
+	@GetMapping("/account/{id}")
+	Accounts getAccountById(@PathVariable Long id) {
+		return accountsRepository.findById(id)
+ 	    	.orElseThrow(() -> new RuntimeException("Account not found with id " + id));
+	}
+
+	@PutMapping("/account/{id}")
+	Accounts updateAccount(@RequestBody Accounts newAccount, @PathVariable Long id) {
+		return accountsRepository.findById(id)
+ 	    	.map(account -> {
+ 	        	account.setUsername(newAccount.getUsername());
+ 				account.setPassword(newAccount.getPassword());
+ 				return accountsRepository.save(account);
+ 	        }).orElseThrow(() -> new RuntimeException("Account not found with id " + id));
+	}
+	
+
+	
+	@DeleteMapping("/account/{id}")
+	String deleteAccount(@PathVariable Long id) {
+		if(!accountsRepository.existsById(id)) {
+ 			throw new RuntimeException("Account not found with id " + id);
+ 		}
+	 	accountsRepository.deleteById(id);
+	 	return "Account with id " + id + " has been deleted successfully.";
+
+	}
+}
---
+++ src/test/java/edu/uscb/csci570sp26/galileo_backend/controller/AccountControllerTest.java
@@ -0,0 +1,16 @@
+package edu.uscb.csci570sp26.galileo_backend.controller;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort; // needed for sorting list of users by ID
+import org.springframework.web.bind.annotation.*;
+
+//import edu.uscb.csci570sp26.galileo_backend.exception.UserNotFoundException;
+import edu.uscb.csci570sp26.galileo_backend.model.Accounts;
+import edu.uscb.csci570sp26.galileo_backend.repository.AccountsRepository;
+
+public class AccountControllerTest {
+
+}
