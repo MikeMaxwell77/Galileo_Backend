7866d11630ab18f8e341a201b7ac57d6960edeac
Daniel <cisco.daniel.scheer@gmail.com>
2026-04-05 17:38:05 -0400

    Implemented everything needed for /auth/register and /auth/login routes. These are linked to the Accounts table. tested and works in postman

--- src/main/java/edu/uscb/csci570sp26/galileo_backend/repository/AccountsRepository.java
+++ src/main/java/edu/uscb/csci570sp26/galileo_backend/repository/AccountsRepository.java
@@ -1,9 +1,11 @@
 package edu.uscb.csci570sp26.galileo_backend.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import edu.uscb.csci570sp26.galileo_backend.model.Accounts;
 
 public interface AccountsRepository extends JpaRepository<Accounts, Long> {
-
+	Optional<Accounts> findByEmail(String email);
 }
