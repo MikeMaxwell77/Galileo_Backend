11279dd05989e0d0e321d80a1ecc88d2a8c9ef23
mm401@email.uscb.edu <118575021+MikeMaxwell77@users.noreply.github.com>
2026-04-19 20:00:29 -0400

    Frontend Production Origin allowed

--- src/main/java/edu/uscb/csci570sp26/galileo_backend/config/SecurityConfig.java
+++ src/main/java/edu/uscb/csci570sp26/galileo_backend/config/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5174", "http://localhost:5173")); // ✅ Allow frontend
+        configuration.setAllowedOrigins(List.of("http://localhost:5174", "http://localhost:5173","https://galileo-uscb.netlify.app/")); // ✅ Allow frontend
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
         configuration.setAllowCredentials(true);
